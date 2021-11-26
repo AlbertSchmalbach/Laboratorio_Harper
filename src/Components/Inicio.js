80d710e7e215fb163f2a793bcaaf064ea6779d22
dylanbarajas <bdylan@uninorte.edu.co>
2021-11-26 16:57:45 -0500

    home

--- src/Components/Inicio.js
+++ src/Components/Inicio.js
@@ -11,7 +11,7 @@ class Inicio extends Component {
     return (
       <div>
         <header>
-          <div className="container-fluid bg-danger shadow-sm">
+          <div className="container-fluid bg-header shadow-sm">
             <div className="row d-flex p-5 ms-2">
               <div className="col">
                 <h2> LABORATORIOS HARPER</h2>
@@ -20,15 +20,15 @@ class Inicio extends Component {
                 <nav className="nav d-flex align-items-center justify-content-center">
                   <li className="nav-item">
                     {" "}
-                    <a className="nav-link ms-5"> HOME </a>{" "}
+                    <a className="nav-link ms-5 active cursor"> HOME </a>{" "}
                   </li>
                   <li className="nav-item">
                     {" "}
-                    <a className="nav-link ms-5"> REGISTRESE </a>{" "}
+                    <a className="nav-link ms-5 text-secondary cursor"> REGISTRESE </a>{" "}
                   </li>
                   <li className="nav-item">
                     {" "}
-                    <a className="nav-link ms-5"> INICIO DE SESION </a>{" "}
+                    <a className="nav-link ms-5 text-secondary cursor"> INICIO DE SESION </a>{" "}
                   </li>
                 </nav>
               </div>
@@ -40,36 +40,21 @@ class Inicio extends Component {
             <img src={Banner} class="img-fluid w-100 h-20 d-inline-block" alt="imagen del banner"/>
           </div>
         </main>
-        <footer className="bg-dark text-center text-white">
+        <footer className=" text-center text-white">
   
-  <div className="container p-4 pb-0">
-    
-    <section className="mb-4">
+        <div className="container d-flex justify-content-around p-4 ">
+          <div className="col">
+            <h1><a className="boton-cita cursor">Agende su cita aquí</a></h1>
+          </div>
+          <div className="col">
+          <button type="button" class="boton">Libro de agendas</button>
+          </div>
 
-      <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i className="fab fa-facebook-f"></i
-      ></a>
-
-      
-  
-    
-      
-      <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      
-      <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i className="fab fa-linkedin-in"></i
-      ></a>
-
-    </section>
-  
-  </div>
-  <div className="text-center p-3">
-    © 2021 Copyright -
-    <a className="text-white" href="#">&nbsp; Laboratorios Harper</a>
-  </div>
+        </div>
+        {/* <div className="text-center p-3">
+          © 2021 Copyright -
+          <a className="text-white" href="#">&nbsp; Laboratorios Harper</a>
+        </div> */}
 
   </footer>
       </div>
